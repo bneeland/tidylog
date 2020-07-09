041285404309e2fd022eeb5c7aca031be6097930
Brian Neeland <brian.neeland@protonmail.com>
2020-07-09 02:53:37 -0600

    Made it full height

--- tidylog/settings.py
+++ tidylog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY_TIDYLOG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'http://127.0.0.1:8000/',
