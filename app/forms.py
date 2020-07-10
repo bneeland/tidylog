37915acf94ce1f90e63bca75a4af9beb1ef92278
Brian Neeland <brian.neeland@protonmail.com>
2020-07-10 16:53:27 -0600

    Correct field labels for new entry form

--- app/forms.py
+++ app/forms.py
@@ -10,21 +10,34 @@ class CreateEntryForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         self.log_pk = kwargs.pop('log_pk', None)
+
         super().__init__(*args, **kwargs)
+
         self.fields.pop('user')
         self.fields.pop('log')
+
         # Get field codes that are applied to Log
         log_qs = models.Log.objects.filter(pk=self.log_pk)
         log = log_qs[0]
         log_fields_qs = log.fields.all()
         log_field_codes = [log_field.type for log_field in log_fields_qs]
+        print("log_field_codes", log_field_codes)
+
         # Get all field codes
         FIELD_BANK = models.Field.FIELD_BANK
         field_codes_all = [fc[0] for fc in FIELD_BANK]
+
         # Remove unneeded field codes
         field_codes_to_pop = [fc for fc in field_codes_all if fc not in log_field_codes]
         for field_code_to_pop in field_codes_to_pop:
             self.fields.pop(field_code_to_pop)
+
+        # # Get list of field labels
+        # field_keys = self.fields.keys()
+
+        # Set field labels
+        for code in log_field_codes:
+            self.fields[code].label = models.Field.objects.filter(type=code)[0].name
 
     def save(self):
         entry = super(CreateEntryForm, self).save(commit=False)
