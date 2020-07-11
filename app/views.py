d393b9251e9db2b2555b38b13f76083eb24162b8
Brian Neeland <brian.neeland@protonmail.com>
2020-07-11 14:39:35 -0600

    Filtered events based on date

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView, UpdateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy, resolve
 from django.views.generic.base import ContextMixin
+import datetime
 
 from . import models
 from . import forms
@@ -43,43 +44,57 @@ class Log(LoginRequiredMixin, AreasMixin, LogsMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         # Get area from URL
         area_pk = self.kwargs['area_pk']
         area = models.Area.objects.get(pk=area_pk)
         context['area'] = area
+
         # Get log from URL
         log_pk = self.kwargs['log_pk']
         log = models.Log.objects.get(pk=log_pk)
         context['log'] = log
+
         # Get headings for current log
         log_fields_qs = log.fields.all()
         context['fields'] = log_fields_qs
+
         # Get entries
         entries = models.Entry.objects.filter(log=log)
+
+        # Filter for 1-day / date-range views
+        url_name = resolve(self.request.path_info).url_name
+
+        if url_name == "log_1_day":
+            year = self.kwargs['year']
+            month = self.kwargs['month']
+            day = self.kwargs['day']
+
+            context['year'] = year
+            context['month'] = month
+            context['day'] = day
+
+            entries = entries.filter(created_at__date=datetime.date(year, month, day))
+
+        if url_name == "log_date_range":
+            year_start = self.kwargs['year_start']
+            month_start = self.kwargs['month_start']
+            day_start = self.kwargs['day_start']
+            year_end = self.kwargs['year_end']
+            month_end = self.kwargs['month_end']
+            day_end = self.kwargs['day_end']
+
+            context['year_start'] = year_start
+            context['month_start'] = month_start
+            context['day_start'] = day_start
+            context['year_end'] = year_end
+            context['month_end'] = month_end
+            context['day_end'] = day_end
+
+            entries = entries.filter(created_at__gte=datetime.date(year_start, month_start, day_start)).filter()
+            entries = entries.filter(created_at__lte=datetime.date(year_end, month_end, day_end+1)).filter()
+
         context['entries'] = entries
-
-        """
-        Let it be known that I spent like 5 hours trying to make this work, and,
-        in the end, there was an easy fix (template tags) that I should have found
-        much earlier. I just didn't think of googling if there was a better way
-        until just now, and them I implemented the right fix in like 5 minutes.
-        """
-        # entries_list = []
-        # i = 0
-        # for log_field in log_fields_qs:
-        #     entry_values = list(entries.values(log_field.type))
-        #     print("entry_values type", type(list(entries.values(log_field.type))))
-        #     if i == 0:
-        #         entries_list = entry_values
-        #     else:
-        #         entries_list = list(zip(entries_list, entry_values))
-        #         print("entries_list type", type(entries_list))
-        #     # entries_list.append(entries.values(log_field.type))
-        #     i += 1
-        # print("entries_list", entries_list)
-        # context['entries'] = entries_list
-
-        # print("ENTRIES_VALUES________", entries.values(log_fields_qs[0].type))
 
         return context
 
@@ -92,24 +107,24 @@ class Log(LoginRequiredMixin, AreasMixin, LogsMixin, CreateView):
         return kwargs
 
     def get_success_url(self):
-        current_url_name = resolve(self.request.path_info).url_name
-        cuffent_url_name_complete = 'app:'+current_url_name
+        url_name = resolve(self.request.path_info).url_name
+        url_name_complete = 'app:'+url_name
 
-        if current_url_name == 'log':
-            return reverse_lazy(cuffent_url_name_complete, kwargs={
+        if url_name == 'log':
+            return reverse_lazy(url_name_complete, kwargs={
                 'area_pk': self.kwargs['area_pk'],
                 'log_pk': self.kwargs['log_pk'],
             })
-        elif current_url_name == 'log_1_day':
-            return reverse_lazy(cuffent_url_name_complete, kwargs={
+        elif url_name == 'log_1_day':
+            return reverse_lazy(url_name_complete, kwargs={
                 'area_pk': self.kwargs['area_pk'],
                 'log_pk': self.kwargs['log_pk'],
                 'year': self.kwargs['year'],
                 'month': self.kwargs['month'],
                 'day': self.kwargs['day'],
             })
-        elif current_url_name == 'log_date_range':
-            return reverse_lazy(cuffent_url_name_complete, kwargs={
+        elif url_name == 'log_date_range':
+            return reverse_lazy(url_name_complete, kwargs={
                 'area_pk': self.kwargs['area_pk'],
                 'log_pk': self.kwargs['log_pk'],
                 'year_start': self.kwargs['year_start'],
@@ -119,7 +134,3 @@ class Log(LoginRequiredMixin, AreasMixin, LogsMixin, CreateView):
                 'month_end': self.kwargs['month_end'],
                 'day_end': self.kwargs['day_end'],
             })
-
-    # def form_valid(self, form):
-    #     form.instance.user = self.request.user
-    #     return super().form_valid(form)
