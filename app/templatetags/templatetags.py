953252076ef191c26fe5586d2e880e47a8156329
Brian Neeland <brian.neeland@protonmail.com>
2020-07-12 11:51:35 -0600

    Converted custom add_remove_day to built-in |add template tag

--- app/templatetags/templatetags.py
+++ app/templatetags/templatetags.py
@@ -5,21 +5,3 @@ register = template.Library()
 @register.filter
 def get_object_attribute(object, attribute):
     return getattr(object, attribute)
-
-@register.filter
-def add_remove_day(day, add_or_remove):
-    if add_or_remove == "add":
-        day += 1
-    elif add_or_remove == "remove":
-        day -= 1
-    return day
-
-# @register.filter
-# def add_day(day):
-#     day += 1
-#     return day
-#
-# @register.filter
-# def remove_day(day):
-#     day -= 1
-#     return day
