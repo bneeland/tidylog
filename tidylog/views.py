65ed9775861e6ffa0fdacae6e93e89fd81522b60
Brian Neeland <brian.neeland@protonmail.com>
2020-08-23 11:31:13 -0600

    Added product page

--- tidylog/views.py
+++ tidylog/views.py
@@ -2,3 +2,12 @@ from django.views.generic.base import TemplateView
 
 class Home(TemplateView):
     template_name = "home.html"
+
+class Product(TemplateView):
+    template_name = "product.html"
+# 
+# class Pricing(TemplateView):
+#     template_name = "pricing.html"
+#
+# class Contact(TemplateView):
+#     template_name = "contact.html"
